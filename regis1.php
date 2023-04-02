4a7ac24d86155c9253d1f662559c214cadd4f8f0
VSL <popart.vl@gmail.com>
2023-04-02 11:58:25 +0700

    summery

--- regis1.php
+++ regis1.php
@@ -18,61 +18,94 @@
     <div class="container">
         <p>Index <a href="index.php">Click here</a></p>
 
-        <div class="mt-3 mb-3 row">
-            <!--// แถว 1 -->
-            <label class="col-sm-3 col-form-label">Customer_ID :</label>
-            <div class="col-sm-10">
-                <input type="text" class="form-control" id="cus_id" value="example">
+        <form name="form1" method="post" action="regis2.php" enctype="multipart/form-data">
+
+            <div class="mt-3 mb-3 row">
+                <!--// แถว 1 -->
+                <label class="col-sm-3 col-form-label">Customer_ID :</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="cus_id" name="cus_id" value="example">
+                </div>
             </div>
-        </div>
-        <div class="mb-3 row">
-            <label class="col-sm-3 col-form-label">Title :</label>
-            <div class="col-sm-10">
-                <input type="text" class="form-control" id="cus_title">
+            <div class="mb-3 row">
+                <label class="col-sm-3 col-form-label">Title :</label>
+                <div class="col-sm-10">
+                    <select class="form-select" id="cus_title" name="cus_title">
+                        <option value="Boy">Boy</option>
+                        <option value="Girl">Girl</option>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                    </select>
+                </div>
             </div>
-        </div>
-        <div class="mb-3 row">
-            <label class="col-sm-3 col-form-label">Name :</label>
-            <div class="col-sm-10">
-                <input type="text" class="form-control" id="cus_fname">
+            <div class="mb-3 row">
+                <label class="col-sm-3 col-form-label">Name :</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="cus_fname" name="cus_fname">
+                </div>
             </div>
-        </div>
-        <div class="mb-3 row">
-            <label class="col-sm-3 col-form-label">Last name :</label>
-            <div class="col-sm-10">
-                <input type="text" class="form-control" id="cus_lname">
+            <div class="mb-3 row">
+                <label class="col-sm-3 col-form-label">Last name :</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="cus_lname" name="cus_lname">
+                </div>
             </div>
-        </div>
-        <div class="mb-3 row">
-            <label class="col-sm-3 col-form-label">Address :</label>
-            <div class="col-sm-10">
-                <input type="text" class="form-control" id="cus_addr">
+            <div class="mb-3 row">
+                <label class="col-sm-3 col-form-label">Address :</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="cus_addr" name="cus_addr">
+                </div>
             </div>
-        </div>
-        <div class="mb-3 row">
-            <label class="col-sm-3 col-form-label">Tel :</label>
-            <div class="col-sm-10">
-                <input type="text" class="form-control" id="cus_tel">
+            <div class="mb-3 row">
+                <label class="col-sm-3 col-form-label">Tel :</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="cus_tel" name="cus_tel">
+                </div>
             </div>
-        </div>
-        <div class="mb-3 row">
-            <label class="col-sm-3 col-form-label">User :</label>
-            <div class="col-sm-10">
-                <input type="text" class="form-control" id="cus_uesr">
+            <div class="mb-3 row">
+                <label class="col-sm-3 col-form-label">User :</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="cus_uesr" name="cus_user">
+                </div>
             </div>
-        </div>
-        <div class="mb-3 row">
-            <label class="col-sm-3 col-form-label">Password :</label>
-            <div class="col-sm-10">
-                <input type="text" class="form-control" id="cus_pass">
+            <div class="mb-3 row">
+                <label class="col-sm-3 col-form-label">Password :</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="cus_pass" name="cus_pass">
+                </div>
             </div>
-        </div>
-        <div class="mb-3 row">
-            <label class="col-sm-3 col-form-label">Picture :</label>
-            <div class="col-sm-10">
-                <input type="text" class="form-control" id="cus_pic">
+            <div class="mb-3 row">
+                <label class="col-sm-3 col-form-label">Picture :</label>
+                <div class="col-sm-10">
+                    <input type="file" class="form-control form-control-sm" id="cus_pic" name="cus_pic"
+                        onchange="previewImage(event)">
+                </div>
             </div>
-        </div>
+            <div class="mb-3 row">  <!--// ฟอร์มแสดงรูปภาพก่อนอัพโหลด -->
+                <label class="col-sm-3 col-form-label">Preview :</label>
+                <div class="col-sm-10">
+                    <img id="imagePreview" style="max-width: 300px; max-height: 300px;">
+                </div>
+            </div>
+
+            <!--//สคริปต์ แสดงรูปภาพก่อนอัพโหลด -->
+            <script>
+            function previewImage(event) {
+                var input = event.target;
+                var reader = new FileReader();
+                reader.onload = function() {
+                    var imagePreview = document.getElementById('imagePreview');
+                    imagePreview.src = reader.result;
+                }
+                reader.readAsDataURL(input.files[0]);
+            }
+            </script>
+            
+
+            <div class="col-auto">
+                <button type="submit" class="btn btn-primary mb-3">Confirm</button>
+            </div>
+        </form>
     </div>
 
     <!-- Optional JavaScript! -->
---
+++ regis2.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Register2!</title>
+</head>
+
+<body>
+
+    <?php
+        $a = $_POST['cus_id'];
+        $b = $_POST['cus_title'];    
+        $c = $_POST['cus_fname'];
+        $d = $_POST['cus_lname'];
+        $e = $_POST['cus_addr'];
+        $f = $_POST['cus_tel'];
+        $g = $_POST['cus_user'];
+        $h = $_POST['cus_pass'];
+        $i = $_FILES["fileField"]["name"];
+        move_uploaded_file($_FILES["fileField"]["tmp_name"], "image/" . $i);
+        require "dataconnect.php";
+        $sql = "INSERT INTO customer (cus_id, cus_title, cus_fname, cus_lname, cus_addr, cus_tel, cus_user, cus_pass, cus_pic) VALUES ('$a', '$b', '$c', '$d', '$e', '$f', '$g', '$h', '$i')";
+        $query = mysqli_query($conn, $sql);
+        
+        if ($query) {
+   ?>
+    <script language="javascript">
+        alert("Add data OK");
+        window.location.replace("show_sess_bs.php");
+    </script>
+    <?php
+        } else {
+    ?>
+    <script language="javascript">
+        alert("Add data Fail");
+        window.history.back();
+    </script>
+    <?php
+        } 
+    ?>
+
+
+    <!-- Optional JavaScript! -->
+    <script src="js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>

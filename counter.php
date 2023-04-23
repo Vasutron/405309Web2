2e8f702a7be16001612c79e4cdd3da905cabf526
VSL <popart.vl@gmail.com>
2023-04-23 09:30:14 +0700

    Create counter.php
    Create counter.php

---
+++ counter.php
@@ -0,0 +1,11 @@
+<?php
+if(isset($counter)){
+    $counter += 1;
+    setcookie("counter", $counter, time() + 3600);
+}else{
+    setcookie("counter", 1, time() + 3600);
+}
+echo "<br><br>";
+echo "คุณเข้าชมเว็บไซต์นี้แล้ว " . $_COOKIE['counter'] . " ครั้ง <br>";
+echo "คุณเข้าชมเว็บไซต์นี้ครั้งล่าสุดเมื่อ " . date("d/m/Y H:i:s", $_COOKIE['counter']);
+?>
